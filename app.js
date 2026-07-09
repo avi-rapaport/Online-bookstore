04d2d2676e7ed28bea37284aaf240efa6bd89c95
avi rapaport <kenblock.ar@gmail.com>
2026-07-09 09:14:02 +0300

    handeled query params From end to end

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import { router as bookRouter } from "./routers/book_router.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/books", bookRouter);
+
+app.get("/", (req, res) => {
+  res.send("Wellcome to my Online bookstore");
+});
+
+app.get("/health", (req, res) => {
+  res.end("server is healthy...");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}...`);
+});
---
+++ routers/book_router.js
@@ -0,0 +1,16 @@
+import { getBooks } from "../services/store_service.js";
+import express from "express";
+
+export const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const { inStock, maxPrice, search } = req.query;
+    const books = await getBooks(inStock, maxPrice, search);
+
+    res.json({ success: true, data: books });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ Success: true, Mesage: "Internal Server Error" });
+  }
+});
